b955ac31cb183e4e95ce574d7a6596185367cf24
Juanfran Correas <jfcorreas@gmail.com>
2020-04-22 22:44:58 +0200

    is_the_same_file function implemented @conflicts

--- storer/storer.py
+++ storer/storer.py
@@ -1,3 +1,4 @@
+import filecmp
 from os import listdir
 from os.path import isfile, join, getsize
 from codetiming import Timer                # https://github.com/realpython/codetiming
@@ -22,28 +23,45 @@ def copyfile_by_blocks(src, dst, block=16384):
                 fdst.write(x)
 
 
+def is_the_same_file(f1, f2):
+    """
+    Verify if two files are the same file
+
+    :param f1: file 1 for comparison
+    :param f2: file 2 for comparison
+    :return: True if the file src and the file dst are the same file (content and stats). If not return False
+    """
+    return filecmp.cmp(f1, f2, False) & filecmp.cmp(f1, f2, True)
+
+
 sourcefiles = [f for f in listdir(SOURCE_PATH)
                if isfile(join(SOURCE_PATH, f))]
 print(r"Número de Ficheros en " + SOURCE_PATH + " = " + str(len(sourcefiles)))
 
 print("Copiando...")
 
-overwrited = 0
+skipped = 0
+renamed = 0
 copiedfiles = 0
 totalsize = 0
 t = Timer(name="class", logger=None)
 t.start()
 for f in sourcefiles:
-    srcpath = join(SOURCE_PATH, f)
-    dstpath = join(DESTINATION_PATH, f)
-    totalsize += getsize(srcpath)
-    if isfile(dstpath):
-        overwrited += 1
-    copyfile_by_blocks(srcpath, dstpath)
+    srcfile = join(SOURCE_PATH, f)
+    dstfile = join(DESTINATION_PATH, f)
+    totalsize += getsize(srcfile)
+    if isfile(dstfile):
+        if is_the_same_file(srcfile, dstfile):
+            skipped += 1
+        else:
+            renamed += 1
+    copyfile_by_blocks(srcfile, dstfile)
     copiedfiles += 1
 elapsed_time = t.stop()
 
 print(str(copiedfiles) + " copiados. Tamaño total: {:.2f}".format(round(totalsize/1024/1024, 2)) + "MBs")
 print("Tiempo transcurrido: {:.4f}".format(round(elapsed_time, 4)) + " segundos")
-print("Ficheros sobrescritos: " + str(overwrited))
+print("Ficheros idénticos (saltar): " + str(skipped))
+print("Ficheros para renombrar: " + str(renamed))
+
 
