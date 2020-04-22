08485c718a492f6e0c75e6f1355df899caa44271
Juanfran Correas <jfcorreas@gmail.com>
2020-04-22 11:24:26 +0200

    Fusion copyfile and copyfile_16kblock functions to: copyfile_by_blocks

--- storer/storer.py
+++ storer/storer.py
@@ -3,20 +3,20 @@ from os.path import isfile, join, getsize
 from codetiming import Timer                # https://github.com/realpython/codetiming
 
 SOURCE_PATH = "/home/jfcm02/Proyectos/Desarrollo/TestData/source_files"
-SOURCE_LARGEFILES_PATH = "/home/jfcm02/Proyectos/Desarrollo/TestData/source_files_large"
 DESTINATION_PATH = "/home/jfcm02/Proyectos/Desarrollo/TestData/dest_files"
 
 
-def copyfile(src, dst):
+def copyfile_by_blocks(src, dst, block=16384):
+    """
+    Copies a file from source to destination
+
+    :param src: source file
+    :param dst: destination file
+    :param block: size of the blocks for the copy iterations
+    """
     with open(src, 'rb') as fsrc:
         with open(dst, 'wb') as fdst:
-            fdst.write(fsrc.read())
-
-
-def copyfile_16kblock(src, dst):
-    with open(src, 'rb') as fsrc:
-        with open(dst, 'wb') as fdst:
-            for x in iter(lambda: fsrc.read(16384), ""):
+            for x in iter(lambda: fsrc.read(block), ""):
                 if not x:
                     break
                 fdst.write(x)
@@ -39,7 +39,7 @@ for f in sourcefiles:
     totalsize += getsize(srcpath)
     if isfile(dstpath):
         overwrited += 1
-    copyfile(srcpath, dstpath)
+    copyfile_by_blocks(srcpath, dstpath)
     copiedfiles += 1
 elapsed_time = t.stop()
 
@@ -47,27 +47,3 @@ print(str(copiedfiles) + " copiados. Tamaño total: {:.2f}".format(round(totalsi
 print("Tiempo transcurrido: {:.4f}".format(round(elapsed_time, 4)) + " segundos")
 print("Ficheros sobrescritos: " + str(overwrited))
 
-sourcefiles = [f for f in listdir(SOURCE_LARGEFILES_PATH)
-               if isfile(join(SOURCE_LARGEFILES_PATH, f))]
-print(r"Número de Ficheros en " + SOURCE_LARGEFILES_PATH + " = " + str(len(sourcefiles)))
-
-print("Copiando...")
-
-overwrited = 0
-copiedfiles = 0
-totalsize = 0
-t = Timer(name="class", logger=None)
-t.start()
-for f in sourcefiles:
-    srcpath = join(SOURCE_LARGEFILES_PATH, f)
-    dstpath = join(DESTINATION_PATH, f)
-    totalsize += getsize(srcpath)
-    if isfile(dstpath):
-        overwrited += 1
-    copyfile_16kblock(srcpath, dstpath)
-    copiedfiles += 1
-elapsed_time = t.stop()
-
-print(str(copiedfiles) + " copiados. Tamaño total: {:.2f}".format(round(totalsize/1024/1024, 2)) + "MBs")
-print("Tiempo transcurrido: {:.4f}".format(round(elapsed_time, 4)) + " segundos")
-print("Ficheros sobrescritos: " + str(overwrited))
