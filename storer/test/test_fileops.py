8e5ff26d6a08a10f7e8cea1e3eb279bae88e3445
jfcorreas <jfcorreas@gmail.com>
2020-05-17 20:10:35 +0200

    Refactor tests @test @fileops

--- storer/test/test_fileops.py
+++ storer/test/test_fileops.py
@@ -4,29 +4,43 @@ from storer.fileops import filecopier
 from shutil import copyfile
 from pathlib import Path
 
-src_files_abs_path = Path().absolute().joinpath('src_files')
-dst_files_abs_path = Path().absolute().joinpath('dst_files')
 
-mock_files = []
-
-
-class TestRenamingFiles(unittest.TestCase):
+class TestFileOps(unittest.TestCase):
     def setUp(self):
-        copyfile(Path(src_files_abs_path).joinpath('file.txt'),
-                 Path(dst_files_abs_path).joinpath('file.txt'))
-        mock_files.append(Path(dst_files_abs_path).joinpath('file.txt'))
-        copyfile(Path(src_files_abs_path).joinpath('file.txt'),
-                 Path(dst_files_abs_path).joinpath('otherfile.txt'))
-        mock_files.append(Path(dst_files_abs_path).joinpath('otherfile.txt'))
-        copyfile(Path(src_files_abs_path).joinpath('file.txt'),
-                 Path(dst_files_abs_path).joinpath('file(1).txt'))
-        mock_files.append(Path(dst_files_abs_path).joinpath('file(1).txt'))
-        copyfile(Path(src_files_abs_path).joinpath('file.txt'),
-                 Path(dst_files_abs_path).joinpath('file'))
-        mock_files.append(Path(dst_files_abs_path).joinpath('file'))
-        copyfile(Path(src_files_abs_path).joinpath('file.txt'),
-                 Path(dst_files_abs_path).joinpath('filefile(1).noextension.txt'))
-        mock_files.append(Path(dst_files_abs_path).joinpath('filefile(1).noextension.txt'))
+        self.src_files_abs_path = Path().absolute().joinpath('storer').joinpath('test').joinpath('src_files')
+        self.dst_files_abs_path = Path().absolute().joinpath('storer').joinpath('test').joinpath('dst_files')
+
+        self.mock_files = []
+        copyfile(Path(self.src_files_abs_path).joinpath('file.txt'),
+                 Path(self.dst_files_abs_path).joinpath('file.txt'))
+        self.mock_files.append(Path(self.dst_files_abs_path).joinpath('file.txt'))
+        copyfile(Path(self.src_files_abs_path).joinpath('file.txt'),
+                 Path(self.dst_files_abs_path).joinpath('otherfile.txt'))
+        self.mock_files.append(Path(self.dst_files_abs_path).joinpath('otherfile.txt'))
+        copyfile(Path(self.src_files_abs_path).joinpath('file.txt'),
+                 Path(self.dst_files_abs_path).joinpath('file(1).txt'))
+        self.mock_files.append(Path(self.dst_files_abs_path).joinpath('file(1).txt'))
+        copyfile(Path(self.src_files_abs_path).joinpath('file.txt'),
+                 Path(self.dst_files_abs_path).joinpath('file'))
+        self.mock_files.append(Path(self.dst_files_abs_path).joinpath('file'))
+        copyfile(Path(self.src_files_abs_path).joinpath('file.txt'),
+                 Path(self.dst_files_abs_path).joinpath('filefile(1).noextension.txt'))
+        self.mock_files.append(Path(self.dst_files_abs_path).joinpath('filefile(1).noextension.txt'))
+
+        copyfile(Path(self.src_files_abs_path).joinpath('file.txt'),
+                 Path(self.dst_files_abs_path).joinpath('othername.txt'))
+        self.mock_files.append(Path(self.dst_files_abs_path).joinpath('othername.txt'))
+        copyfile(Path(self.src_files_abs_path).joinpath('file.txt'),
+                 Path(self.dst_files_abs_path).joinpath('distinctfile.txt'))
+        self.mock_files.append(Path(self.dst_files_abs_path).joinpath('distinctfile.txt'))
+
+    def tearDown(self):
+        for f in self.mock_files:
+            Path.unlink(f)
+        self.mock_files.clear()
+
+
+class TestRenamingFiles(TestFileOps):
 
     def test_add_str_to_filename(self):
         self.assertEqual(
@@ -42,34 +56,40 @@ class TestRenamingFiles(unittest.TestCase):
     def test_sequential_filename(self):
         self.assertEqual(
             filecopier.sequential_filename(
-                Path(dst_files_abs_path).joinpath('otherfile.txt')),
-            str(Path(dst_files_abs_path).joinpath('otherfile(1).txt')),
+                Path(self.dst_files_abs_path).joinpath('otherfile.txt')),
+            str(Path(self.dst_files_abs_path).joinpath('otherfile(1).txt')),
             'File with extension')
         self.assertEqual(
             filecopier.sequential_filename(
-                Path(dst_files_abs_path).joinpath('file.txt')),
-            str(Path(dst_files_abs_path).joinpath('file(2).txt')),
+                Path(self.dst_files_abs_path).joinpath('file.txt')),
+            str(Path(self.dst_files_abs_path).joinpath('file(2).txt')),
             'File with extension previously renamed')
         self.assertEqual(
             filecopier.sequential_filename(
-                Path(dst_files_abs_path).joinpath('file(1).txt')),
-            str(Path(dst_files_abs_path).joinpath('file(1)(1).txt')),
+                Path(self.dst_files_abs_path).joinpath('file(1).txt')),
+            str(Path(self.dst_files_abs_path).joinpath('file(1)(1).txt')),
             'File with sequential number in original name')
         self.assertEqual(
             filecopier.sequential_filename(
-                Path(dst_files_abs_path).joinpath('file')),
-            str(Path(dst_files_abs_path).joinpath('file(1)')),
+                Path(self.dst_files_abs_path).joinpath('file')),
+            str(Path(self.dst_files_abs_path).joinpath('file(1)')),
             'File without extension')
         self.assertEqual(
             filecopier.sequential_filename(
-                Path(dst_files_abs_path).joinpath('filefile(1).noextension.txt')),
-            str(Path(dst_files_abs_path).joinpath('filefile(1).noextension(1).txt')),
+                Path(self.dst_files_abs_path).joinpath('filefile(1).noextension.txt')),
+            str(Path(self.dst_files_abs_path).joinpath('filefile(1).noextension(1).txt')),
             'File with dots before extension')
 
-    def tearDown(self):
-        for f in mock_files:
-            Path.unlink(f)
-        mock_files.clear()
+
+class TestIsTheSameFile(TestFileOps):
+
+    def test_is_the_same_file(self):
+        self.assertEqual(
+            filecopier.is_the_same_file(
+                Path(self.src_files_abs_path).joinpath('file.txt'),
+                Path(self.dst_files_abs_path).joinpath('file.txt')),
+            True,
+            'File with extension')
 
 
 if __name__ == '__main__':
